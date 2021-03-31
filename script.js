0a3801d545fd816da08c6517cb3218b13cea6e73
Carlos Hughes <carlos_hughes@hotmail.com>
2021-03-31 15:22:36 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function draw() {
+
+  var colors = ["red", "blue", "yellow", "deepskyblue"];
+
+
+}
+
+function refresh() {
+
+    setTimeout(function () {
+        location.reload()
+    }, 500);
+
+
+}
